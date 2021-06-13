f8f5e5d3307583ea54d978273f8800f9dc5e960e
Arne Kepp <git@kepp.net>
2021-06-13 12:38:49 +0200

    Enable upload of image, prefix "dp." in queries

--- production/build.sh
+++ production/build.sh
@@ -3,5 +3,5 @@ NAME=mapserver
 HASH=$(git log --pretty=format:'%h' -n 1)
 REGISTRY=eu.gcr.io/cloudmark2
 docker build --no-cache  -t ${REGISTRY}/${NAME}:latest -t ${REGISTRY}/${NAME}:${HASH} .
-#docker push ${REGISTRY}/${NAME}:latest
-#docker push ${REGISTRY}/${NAME}:${HASH}
+docker push ${REGISTRY}/${NAME}:latest
+docker push ${REGISTRY}/${NAME}:${HASH}
